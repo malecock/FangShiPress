b6ca94b695f5093ac48b7e760870b26a6b2e5be4
malecock <mattalecock@gmail.com>
2013-08-27 22:44:17 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Press::Application.config.secret_token = '70e13f93385f455721f0e741792ea9585ecd9840f925a4d28e59d5d8357735b661ce13f77ff73297dbf6ba4e61be1b7369ef741c9c8ef4a152c87bc42ece966f'
